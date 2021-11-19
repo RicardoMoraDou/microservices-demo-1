d43d09541b7286a90bcab575f06809bcc41dac4e
IsaacBezares <isaacbezares@IsaacBezares-MacBook-Pro.local>
2021-11-19 16:39:31 -0600

    add backend storage and remove role assignment for aks

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -1,13 +1,19 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform"
+    storage_account_name = "hipsterteamfive"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
   required_providers {
       azurerm = {
         source = "hashicorp/azurerm"
         version = "=2.46.0"
       }
-
+  }
     //   kubernetes = {
     //       source = "hashicorp/kubernetes"
     //       version = "=2.6.1"
     //   }
-  }
 }
