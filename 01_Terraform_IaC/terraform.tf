bde2e72a6be9004be2a82062c8ae9adbc73e9244
IsaacBezares <isaacbezares@IsaacBezares-MacBook-Pro.local>
2021-11-23 17:45:25 -0600

    change to bank of anthos

--- 01_Terraform_IaC/terraform.tf
+++ 01_Terraform_IaC/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform"
-    storage_account_name = "hipsterteamfive"
+    storage_account_name = "anthosteamfive"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
